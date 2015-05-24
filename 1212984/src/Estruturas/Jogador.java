0f4d1f4b61dfbe2b4de978429be79ea59716d8a6
Hugo Costa Machado <hugocm93@gmail.com>
2015-05-24 02:41:20 -0300

    Populando mundo

--- 1212984/src/Estruturas/Jogador.java
+++ 1212984/src/Estruturas/Jogador.java
@@ -14,4 +14,46 @@ public class Jogador {
 			
 			return 0;
 		}
+
+		public Cor getCor() {
+			return cor;
+		}
+
+		public void setCor(Cor cor) {
+			this.cor = cor;
+		}
+
+		public Objetivo getObj() {
+			return obj;
+		}
+
+		public void setObj(Objetivo obj) {
+			this.obj = obj;
+		}
+
+		public Carta[] getCartas() {
+			return cartas;
+		}
+
+		public void setCartas(Carta[] cartas) {
+			this.cartas = cartas;
+		}
+
+		public Territorio[] getTerritoriosPossuidos() {
+			return territoriosPossuidos;
+		}
+
+		public void setTerritoriosPossuidos(Territorio[] territoriosPossuidos) {
+			this.territoriosPossuidos = territoriosPossuidos;
+		}
+
+		public Continente[] getContinentesPossuidos() {
+			return continentesPossuidos;
+		}
+
+		public void setContinentesPossuidos(Continente[] continentesPossuidos) {
+			this.continentesPossuidos = continentesPossuidos;
+		}
+		
+		
 }
